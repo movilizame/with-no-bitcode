8762c0f20fc18cce463210108d98ab739eac09c5
Pablo Botta <pabloluisbotta@gmail.com>
2022-01-21 16:46:35 -0300

    custom 0.3.3

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -4,16 +4,13 @@ import {
   WarningAggregator
 } from "@expo/config-plugins";
 
-const pkg = { name: "react-native-webrtc", version: "UNVERSIONED" }; //require("react-native-webrtc/package.json");
+const pkg = { name: "react-native-webrtc-custom", version: "1" }; //require("react-native-webrtc/package.json");
 
 const withNoBitcode: ConfigPlugin<void> = (config) => {
   if (!config.ios) {
     config.ios = {};
   }
-  WarningAggregator.addWarningIOS(
-    "ios.bitcode",
-    'Fully disabled it'
-  );
+  WarningAggregator.addWarningIOS("ios.bitcode", "Fully disabled it");
   // WebRTC requires Bitcode be disabled for
   // production iOS builds that target devices, e.g. not simulators.
   // SDK +44 property
